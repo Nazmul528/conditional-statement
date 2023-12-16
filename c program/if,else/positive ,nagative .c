4560981518a7d852f5864c2fa72bc7da17b70ef8
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-12-16 10:11:59 +0600

    Create positive ,nagative .c

---
+++ c program/if,else/positive ,nagative .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    if (n>0)
+        printf("%d number positive",n);
+    else
+        printf("%d number nagative",n);
+}
