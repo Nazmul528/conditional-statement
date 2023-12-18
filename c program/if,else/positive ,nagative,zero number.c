08a526597f27876b9e1590b3fc7f3c476125df2d
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-12-18 07:49:18 +0600

    Create positive ,nagative,zero number.c

---
+++ c program/if,else/positive ,nagative,zero number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    if(a>0)
+    printf("the positive number");
+    else if (a<0)
+    printf("the nagative number");
+    else
+    printf(" the zero");
+    return 0;
+}    
