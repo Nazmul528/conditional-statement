f1a07ce2cd938ce502e47ca2c5ab016753e14c22
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-12-21 21:32:08 +0600

    Update and rename .c to 1 to 9 print.c

---
+++ switch/1 to 9 print.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int dight;
+    printf("Enter a dight :");
+    scanf("%d",&dight);
+    switch(dight)
+    {
+    case 0:
+    printf("the number zero\n");
+    break;
+    case 1:
+    printf("the number one\n");
+    break;
+    case 2:
+    printf("the number two\n");
+    break;
+    case 3:
+    printf("the number three\n");
+    break;
+    case 4:
+    printf("the number four\n");
+    break;
+    case 5:
+    printf("the number five\n");
+    break;
+    case 6:
+    printf("the number six\n");
+    break;
+    case 7:
+    printf("the number seven\n");
+    break;
+    case 8:
+    printf("the number eight\n");
+    break;
+    case 9:
+    printf("the number nine\n");
+    break;
+    default:
+    printf("not valit dight");
+    }
+}    
